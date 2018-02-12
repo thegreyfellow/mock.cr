8940166dfcdf3f4c9f733ff5d6ed1f9492821d58
Youssef Idmoussi <youssef.idmoussi@gmail.com>
2018-02-12 23:50:24 +0000

    Update test phrase.

--- spec/morse_spec.cr
+++ spec/morse_spec.cr
@@ -2,13 +2,13 @@ require "./spec_helper"
 
 describe Morse do
 
-  to_encode = "hello world, this youssef from the past."
+  to_encode = "hello world, this is me from the past."
   it "encode" do
-    Morse.encode(to_encode).should eq(".... . .−.. .−.. −−− / .−− −−− .−. .−.. −.. −−..−− / − .... .. ... / −.−− −−− ..− ... ... . ..−. / ..−. .−. −−− −− / − .... . / .−−. .− ... − .−.−.−")
+    Morse.encode(to_encode).should eq(".... . .−.. .−.. −−− / .−− −−− .−. .−.. −.. −−..−− / − .... .. ... / .. ... / −− . / ..−. .−. −−− −− / − .... . / .−−. .− ... − .−.−.−")
   end
 
-  to_decode = ".... . .−.. .−.. −−− / .−− −−− .−. .−.. −.. −−..−− / − .... .. ... / −.−− −−− ..− ... ... . ..−. / ..−. .−. −−− −− / − .... . / .−−. .− ... − .−.−.−"
+  to_decode = ".... . .−.. .−.. −−− / .−− −−− .−. .−.. −.. −−..−− / − .... .. ... / .. ... / −− . / ..−. .−. −−− −− / − .... . / .−−. .− ... − .−.−.−"
   it "decode" do
-    Morse.decode(to_decode).should eq("hello world, this youssef from the past.")
+    Morse.decode(to_decode).should eq("hello world, this is me from the past.")
   end
 end
